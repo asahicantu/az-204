4385516c8e98f952fcb93559fa6301c760dc84dc
Asahi Cantu <asahicantu@outlook.com>
2023-01-21 23:07:49 +0100

    implemented aure storage accounts scripting

---
+++ script/createBlobStorageAccount.ps1
@@ -0,0 +1,4 @@
+$rg='az204-blob-rg'
+$san='mystore203122141'
+az group create --location norwayeast --name $rg
+az storage account create -g $rg --name $san --location norwayeast --kind BlockBlobStorage  --sku Premium_LR
