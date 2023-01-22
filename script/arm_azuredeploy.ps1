567de37ce8fa4182f9310792da09b2a799716ac9
Asahi Cantu <asahicantu@outlook.com>
2023-01-22 14:13:11 +0100

    changes to arm template

--- script/arm_azuredeploy.ps1
+++ script/arm_azuredeploy.ps1
@@ -1,6 +1,6 @@
 $rg='az204-arm-rg'
 $todayDate = Get-Date -Format "yyyMMddhhmm"
-$san='az204storageacctarm$todayDate'
+$san='az204storageacctarm20230122'
 #az login
 az group create -g $rg --location norwayeast
 az deployment group create -g $rg --template-file ../arm/azuredeploy.json --parameters  ../arm/azuredeploy.parameters.json
